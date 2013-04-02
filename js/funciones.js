02eb0e3a2024e07ac2bb6b3933daca815ed5e71c
Neyber Becerra Zapata <enjey11@gmail.com>
2013-04-02 17:01:25 -0500

    Cambios 02-04-2013
    Avances de El Taller MP

--- js/funciones.js
+++ js/funciones.js
@@ -37,6 +37,17 @@ $(document).ready(function(){
 	$(window).resize(function(){
 		RepositionNav();
 	});*/	
+
+	$('.carousel').carousel({
+		carouselWidth:726,
+		carouselHeight:239,
+		frontWidth:239,
+		frontHeight:239,
+		hMargin:0.9,
+		slidesPerScroll:3,
+		directionNav:true,
+		shadow:false
+	});
 	
 	//.parallax(xPosition, adjuster, inertia, outerHeight) options:
 	//xPosition - Horizontal position of the element
@@ -52,6 +63,10 @@ $(document).ready(function(){
 
 	$('#mouseScroll').one('mouseenter',function(){
 		$(this).sprite({fps: 10, no_of_frames: 7});
+	});
+
+	$('#mouseScroll1').one('mouseenter',function(){
+		$(this).sprite({fps: 10, no_of_frames: 5});
 	});
 	
 	var deck = new $.scrolldeck({
@@ -71,59 +86,6 @@ $(document).ready(function(){
 	$('#top-link').click(function(e) {
 		e.preventDefault();
 		$.scrollTo(0,900);
-	});
-
-
-
-	/******************************* FILTRO DE IMAGENES */
-
-	//comenzamos aplicandole un evento click a las etiquetas de tipo ancla
-	//que se encuentran en la lista (todos, rojos, azules, amarillo)
-	$('#filter ul li a').click(function() {
-		
-		$('#filter ul li a').removeClass('active');
-		$(this).addClass('active');
-		//el texto al cual dimos click lo pasamos a minusculas, 
-		//le quitamos los espacios en blanco y lo asignamos a la variable
-		//textoFiltro
-		var textoFiltro = $(this).find('span').text().toLowerCase().replace(/\s/g,"-");
-		
-		//si el texto es igual a 'todos' mostramos todos los elementos que contengan la clase hidden
-		//y a dicho elemento le removemos la clase hidden
-		//la clase hidden es opcional, en éste caso la usamos solo como referencia
-		//puedes llamarla como quieras o incluso no utilizarla
-		if(textoFiltro == 'todos') 
-		{
-			$('div#filtros div.hidden')/*.fadeIn('slow')*/.removeClass('hidden');
-		}
-		//de lo contrario hacemos lo siguiente
-		else
-		{
-			//aqui empieza la magia
-			
-			//hacemos un bucle con el metodo each para
-			//obtener todos los divs que se encuentren dentro de 
-			//la clase filtros
-			$('#filtros div').each(function() {
-				
-				//entonces comparamos
-				//si el elemento NO tiene una clase llamada con el mismo valor que
-				//nuestra variable textoFiltro, entonces se ocultará utilizando el metodo
-				//fadeOut() de jQuery
-				if(!$(this).hasClass(textoFiltro)) 
-				{
-					$(this).show('slow').addClass('hidden');
-				}
-				//de lo contrario se mostrará utilizando el método
-				//fadeIn() de jQuery
-				else 
-				{
-					$(this).show('slow').removeClass('hidden');
-				}
-			});
-		}
-		
-		return false;
 	});
 
 	/****************************************** POPUP */
